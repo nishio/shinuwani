83989184b4c5d83a36bbb5a3dc532834544b59ab
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-02-08 11:13:48 +0900

    split component

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import FormLabel from '@material-ui/core/FormLabel';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import React, { useState } from 'react';
+import React from 'react';
+import { getGlobal, setGlobal, useGlobal } from "reactn";
 import './App.css';
-import { useGlobal, getGlobal, setGlobal } from "reactn";
-import { MALE, FEMALE } from './LifeData';
+import { FEMALE, MALE } from './LifeData';
 import { Share } from './Share';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -28,10 +28,23 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+const INITIAL_STATE = {
+  gender: "male",
+  luckS: "0.01",
+  startAgeS: "38",
+}
+type TYPE_GLOBAL_STATE = typeof INITIAL_STATE
+declare module 'reactn/default' {
+  export interface State extends TYPE_GLOBAL_STATE { }
+}
+setGlobal(INITIAL_STATE)
+setGlobal({ luck: "string" });
+
 const App = () => {
-  const [gender, setGender] = useState('male');
-  const [luckS, setLuck] = useState("0.01")
-  const [startAgeS, setAge] = useState("38")
+  const [gender] = useGlobal('gender');
+  const [luckS] = useGlobal("luckS")
+  const [startAgeS] = useGlobal("startAgeS")
+
   const startAge = parseInt(startAgeS);
   const luck = parseFloat(luckS);
   let alive = 1.0;
@@ -54,15 +67,6 @@ const App = () => {
     }
   }
 
-  const changeGender = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setGender((event.target as HTMLInputElement).value);
-  };
-  const chengeLuck = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLuck((event.target as HTMLInputElement).value);
-  };
-  const changeAge = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setAge((event.target as HTMLInputElement).value);
-  };
   let genderString;
   if (gender === "male") {
     genderString = "男性"
@@ -73,60 +77,12 @@ const App = () => {
   }
   const result = `あなたは${age - startAge}年後に${age}歳で死にます。`;
 
-  const classes = useStyles();
-  const ageList = [];
-  for (let i = 0; i < 101; i++) {
-    ageList.push(<MenuItem value={i}>{i}</MenuItem>)
-  }
-
   return (
     <div className="App">
       <h1>○年後に死ぬあなた</h1>
       漫画「100年後に死ぬワニ」を見てたら自分が何年後に死ぬのか気になったので作りました。
 
-      <div className="form">
-        <Grid item xs={12}>
-          <Grid container justify="center">
-            <Grid>
-              <FormControl component="fieldset" className={classes.formControl}>
-                <FormLabel component="legend" className={classes.formLabel}>あなたの今の年齢</FormLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={startAge}
-                  onChange={changeAge}
-                  className={classes.select}
-                >
-                  {ageList}
-                </Select>
-              </FormControl>
-
-            </Grid>
-            <Grid>
-              <FormControl component="fieldset" className={classes.formControl}>
-                <FormLabel component="legend" className={classes.formLabel}>あなたの性別</FormLabel>
-                <RadioGroup aria-label="gender" name="gender" value={gender} onChange={changeGender}>
-                  <FormControlLabel value="male" control={<Radio />} label="男性" />
-                  <FormControlLabel value="female" control={<Radio />} label="女性" />
-                  <FormControlLabel value="other" control={<Radio />} label="平均" />
-                </RadioGroup>
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl component="fieldset" className={classes.formControl}>
-                <FormLabel component="legend" className={classes.formLabel}>あなたの運</FormLabel>
-                <RadioGroup aria-label="luck" name="luck" value={luckS} onChange={chengeLuck}>
-                  <FormControlLabel value="0.001" control={<Radio />} label="1000人に1人の悪さ" />
-                  <FormControlLabel value="0.01" control={<Radio />} label="100人に1人の悪さ" />
-                  <FormControlLabel value="0.1" control={<Radio />} label="10人に1人の悪さ" />
-                  <FormControlLabel value="0.25" control={<Radio />} label="4人に1人の悪さ" />
-                  <FormControlLabel value="0.5" control={<Radio />} label="普通" />
-                </RadioGroup>
-              </FormControl>
-            </Grid>
-          </Grid>
-        </Grid>
-      </div>
+      <Form />
 
       <p className="result">{result}</p>
 
@@ -144,7 +100,66 @@ const App = () => {
 }
 
 const Form = () => {
+  const classes = useStyles();
+  const ageList = [];
+  for (let i = 0; i < 101; i++) {
+    ageList.push(<MenuItem value={i}>{i}</MenuItem>)
+  }
+  const global = getGlobal();
 
-  return <></>
-}
+  const changeGender = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGlobal({ gender: (event.target as HTMLInputElement).value });
+  };
+
+  const chengeLuck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGlobal({ luckS: (event.target as HTMLInputElement).value });
+  };
+  const changeAge = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setGlobal({ startAgeS: (event.target as HTMLInputElement).value })
+  };
+  return <div className="form">
+    <Grid item xs={12}>
+      <Grid container justify="center">
+        <Grid>
+          <FormControl component="fieldset" className={classes.formControl}>
+            <FormLabel component="legend" className={classes.formLabel}>あなたの今の年齢</FormLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={global.startAgeS}
+              onChange={changeAge}
+              className={classes.select}
+            >
+              {ageList}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid>
+          <FormControl component="fieldset" className={classes.formControl}>
+            <FormLabel component="legend" className={classes.formLabel}>あなたの性別</FormLabel>
+            <RadioGroup aria-label="gender" name="gender"
+              value={global.gender} onChange={changeGender}>
+              <FormControlLabel value="male" control={<Radio />} label="男性" />
+              <FormControlLabel value="female" control={<Radio />} label="女性" />
+              <FormControlLabel value="other" control={<Radio />} label="平均" />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid>
+          <FormControl component="fieldset" className={classes.formControl}>
+            <FormLabel component="legend" className={classes.formLabel}>あなたの運</FormLabel>
+            <RadioGroup aria-label="luck" name="luck" value={global.luckS} onChange={chengeLuck}>
+              <FormControlLabel value="0.001" control={<Radio />} label="1000人に1人の悪さ" />
+              <FormControlLabel value="0.01" control={<Radio />} label="100人に1人の悪さ" />
+              <FormControlLabel value="0.1" control={<Radio />} label="10人に1人の悪さ" />
+              <FormControlLabel value="0.25" control={<Radio />} label="4人に1人の悪さ" />
+              <FormControlLabel value="0.5" control={<Radio />} label="普通" />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+      </Grid>
+    </Grid>
+  </div>
+};
+
 export default App;
